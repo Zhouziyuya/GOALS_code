67269dcce7fae9f68b054a2297b1aeb9a5df7aeb
Zhouziyuya <2711545965@qq.com>
2022-06-26 21:58:17 +0800

    完成训练的所有代码，但还有个bug

--- task2.py
+++ task2.py
@@ -10,7 +10,7 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import LabelBinarizer
 from sklearn.metrics import auc
-from torch.utils.data import Dataset
+from torch.utils.data import Dataset, DataLoader
 
 import torch
 import torch.nn as nn
@@ -25,19 +25,22 @@ warnings.filterwarnings('ignore')
 # 配置
 batchsize = 8 # 批大小,
 image_size = 256
-iters = 1000 # 迭代次数
+iters = 100 # 迭代次数
 val_ratio = 0.2 # 训练/验证数据划分比例，80 / 20
-trainset_root = "../datasets/Train/Image"
-val_root = "../datasets/Train/Image"
-test_root = "../datasets/Validation/Image"
+trainset_root = '../datasets/Train/Image'
+val_root = '../datasets/Train/Image'
+train_label_root = '../datasets/Train/Train_GC_GT.xlsx'
+test_root = '../datasets/Validation/Image'
 num_workers = 4
 init_lr = 1e-6
 optimizer_type = 'adam'
+device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
 
 # 训练/验证数据集划分
 filelists = os.listdir(trainset_root)
 train_filelists, val_filelists = train_test_split(filelists, test_size=val_ratio, random_state=42)
 print("Total Nums: {}, train: {}, val: {}".format(len(filelists), len(train_filelists), len(val_filelists)))
+print(val_filelists)
 
 # 数据加载（继承torch.utils.data中的Dataset类）
 class GOALS_sub2_dataset(Dataset):
@@ -48,21 +51,21 @@ class GOALS_sub2_dataset(Dataset):
                 filelists=None,
                 numclasses=2,
                 mode='train'):
-        self.dataset_root = dataset_root
-        self.img_transforms = img_transforms
-        self.mode = mode.lower()
-        self.num_classes = numclasses
+                self.dataset_root = dataset_root
+                self.img_transforms = img_transforms
+                self.mode = mode.lower()
+                self.num_classes = numclasses
 
-        if self.mode == 'train':
-            label = {row['ImgName']:row[1]
-                    for _, row in pd.read_excel(label_file).iterrows()}
-            self.file_list = [[f, label[int(f.split('.')[0])]] for f in os.listdir(dataset_root)]
+                if self.mode == 'train': # 如果输入了mode，train或test，数据集为dataset_root中图片
+                    label = {row['ImgName']:row[1]
+                            for _, row in pd.read_excel(label_file).iterrows()}
+                    self.file_list = [[f, label[int(f.split('.')[0])]] for f in os.listdir(dataset_root)]
 
-        elif self.mode == "test":
-            self.file_list = [[f, None] for f in os.listdir(dataset_root)]
-        
-        if filelists is not None:
-            self.file_list = [item for item in self.file_list if item[0] in filelists]
+                elif self.mode == "test":
+                    self.file_list = [[f, None] for f in os.listdir(dataset_root)]
+                
+                if filelists is not None:  # 如果输入filelists，数据集为filelists中图片,dataset_root不起作用
+                    self.file_list = [item for item in self.file_list if item[0] in filelists]
     
     def __getitem__(self, idx):
 
@@ -75,8 +78,8 @@ class GOALS_sub2_dataset(Dataset):
  
         # normlize on GPU to save CPU Memory and IO consuming.
         # img = (img / 255.).astype("float32")
-
-        img = img.transpose(2, 0, 1) # H, W, C -> C, H, W
+        # print(img.shape)
+        # img = img.transpose(2, 0).transpose(1,2) # H, W, C -> C, H, W
 
         if self.mode == 'test':
             return img, real_index
@@ -86,6 +89,127 @@ class GOALS_sub2_dataset(Dataset):
 
     def __len__(self):
         return len(self.file_list)
+
+# plot loss and accuracy
+def plot_loss(loss):
+    plt.figure()
+    loss = np.array(torch.tensor(loss, device='cpu'))
+    plt.plot(loss, 'b-', label = 'val loss')
+    plt.legend(fontsize=10)
+    plt.title('loss', fontsize = 10)
+    plt.xlabel('Iteration/100')
+    plt.savefig('./figures/task2/loss.png')
+
+def plot_accuracy(acc):
+    plt.figure()
+    acc = np.array(acc)
+    plt.plot(acc, 'b-', label = 'val acc')
+    plt.legend(fontsize=10)
+    plt.title('accuracy', fontsize = 10)
+    plt.xlabel('Iteration/100')
+    plt.savefig('./figures/task2/accuracy.png')
+
+# 网络模型
+class Model(nn.Module):
+    def __init__(self):
+        super(Model, self).__init__()
+        self.feature = resnet50(pretrained=True) # 移除最后一层全连接
+        # self.feature = resnet101(pretrained=True, num_classes=2) # 移除最后一层全连接
+        self.fc1 = nn.Linear(2048, 1024)
+        self.fc2 = nn.Linear(1024, 2)
+
+    def forward(self, img):
+        feature = self. feature(img)
+        feature = torch.flatten(feature, 1)
+        out1 = self.fc1(feature)
+        logit = self.fc2(out1)
+
+        return logit
+
+# 功能函数
+def train(model, iters, train_dataloader, val_dataloader, optimizer, criterion, log_interval, eval_interval, device):
+    iter = 0
+    model.train()
+    avg_loss_list = []
+    avg_acc_list = []
+    best_acc = 0.
+    n_correct = 0
+    n_total = 0
+    while iter < iters:
+        for data in train_dataloader: # 便利dataloader得到的data为一个batch里的图像和label
+            iter += 1
+            if iter > iters:
+                break
+            imgs = (data[0] / 255.).to(torch.float32).to(device)
+            labels = data[1].to(torch.int64).to(device)
+            # print(labels)
+            # labels_ = torch.unsqueeze(labels, axis=1) # 维度扩展：一维80-->二维80*1
+            logits = model(imgs) 
+            # m = paddle.nn.Softmax()
+            # pred = m(logits)
+            # print(pred.numpy())
+            # print(pred.numpy().argmax(1))            
+            # acc = paddle.metric.accuracy(input=pred, label=labels_)
+            _, indices = logits.max(dim=1) # 找出行最大值，返回索引
+            n_correct += sum(indices==labels)
+            n_total += len(labels)
+            acc = n_correct.cpu().detach().numpy() * 1.0 /n_total
+            # one_hot_labels = paddle.fluid.layers.one_hot(labels_, 2, allow_out_of_range=False)
+            loss = criterion(logits, labels)            
+            # print(loss.numpy())
+            loss.backward()
+            optimizer.step()
+            optimizer.zero_grad()
+
+
+            # model.clear_gradients()
+            avg_loss_list.append(loss.numpy()[0])
+            avg_acc_list.append(acc.numpy())
+            
+
+            if iter % log_interval == 0:
+                avg_loss = np.array(avg_loss_list).mean()
+                avg_acc = np.array(avg_acc_list).mean()
+                avg_loss_list = [] # 存log_interval个iter的loss和acc
+                avg_acc_list = []
+                print('[TRAIN] iter={}/{} avg_loss={:.4f} avg_acc={:.4f}'.format(iter, iters, avg_loss, avg_acc))
+
+            if iter % eval_interval == 0:
+                avg_loss, avg_acc = val(model, val_dataloader, criterion)
+                print('[EVAL] iter={}/{} avg_loss={:.4f} acc={:.4f}'.format(iter, iters, avg_loss, avg_acc))
+                if avg_acc >= best_acc:
+                    best_acc = avg_acc
+                    torch.save(model.state_dict(), '/home/zhouziyu/miccai2022challenge/GOALS_code/task2_models/train1.pth')
+                model.train()
+
+def val(model, val_dataloader, criterion):
+    model.eval()
+    loss_list = []
+    acc_list = []
+    n_correct = 0
+    n_total = 0
+    with torch.no_grad():
+        for data in val_dataloader:
+            imgs = (data[0] / 255.).astype('float32')
+            labels = data[1].astype('int64')
+            logits = model(imgs)
+            _, indices = logits.max(dim=1) # 找出行最大值，返回索引
+            n_correct += sum(indices==labels)
+            n_total += len(labels)
+            acc = n_correct.cpu().detach().numpy() * 1.0 /n_total
+            loss = criterion(logits, labels)     
+            loss_list.append(loss.numpy()[0])
+            acc_list.append(acc.numpy()) 
+
+    avg_loss = np.array(loss_list).mean()
+    avg_acc = np.array(acc_list).mean()
+    plot_loss(loss_list)
+    plot_accuracy(acc_list)
+
+    return avg_loss, avg_acc      
+
+
+# 训练阶段
 
 # 数据增强
 img_train_transforms = trans.Compose([
@@ -98,200 +222,67 @@ img_train_transforms = trans.Compose([
 ])
 
 img_val_transforms = trans.Compose([
-    trans.CropCenterSquare(),
+    trans.ToTensor(),
+    # trans.CenterCrop(image_size),
     trans.Resize((image_size, image_size))
 ])
 
-# 可视化并检查数据加载
-_train = GOALS_sub2_dataset(dataset_root=trainset_root, 
-                        img_transforms=img_train_transforms,
-                        label_file='../datasets/Train/Train_GC_GT.xlsx')
+train_dataset = GOALS_sub2_dataset(img_transforms=img_train_transforms,
+                                    dataset_root=trainset_root,
+                                    label_file=train_label_root,
+                                    filelists=train_filelists)
 
-plt.figure(figsize=(15, 5))
+val_dataset = GOALS_sub2_dataset(img_transforms=img_val_transforms,
+                                dataset_root=trainset_root,
+                                label_file=train_label_root,
+                                filelists=val_filelists)
 
-for i in range(5):
-    img, lab = _train.__getitem__(i)
-    plt.subplot(1, 5, i+1)
-    plt.imshow(img.transpose(1, 2, 0))
-    plt.axis("off")
+train_loader = DataLoader(dataset=train_dataset,
+                        batch_size=batchsize,
+                        num_workers=num_workers,
+                        shuffle=True,
+                        drop_last=True)
 
-_val = GOALS_sub2_dataset(dataset_root=trainset_root, 
-                        img_transforms=img_val_transforms,
-                        label_file='../datasets/Train/Train_GC_GT.xlsx')
-
-plt.figure(figsize=(15, 5))
-for i in range(5):
-    img, lab = _val.__getitem__(i)
-    plt.subplot(1, 5, i+1)
-    plt.imshow(img.transpose(1, 2, 0))
-    plt.axis("off")
+val_loader = DataLoader(dataset=val_dataset,
+                        batch_size=batchsize,
+                        num_workers=num_workers,
+                        shuffle=True,
+                        drop_last=True)
 
 
-# 网络模型
-class Model(nn.Module):
-    def __init__(self):
-        super(Model, self).__init__()
-        self.feature = resnet50(pretrained=True, num_classes=2) # 移除最后一层全连接
-        # self.feature = resnet101(pretrained=True, num_classes=2) # 移除最后一层全连接
-        self.fc1 = nn.Linear(2048, 1024)
-        self.fc2 = nn.Linear(1024, 2)
+model = Model().to(device)
 
-    def forward(self, img):
-        feature = self. feature(img)
-        out1 = self.fc1(feature)
-        logit = self.fc2(out1)
+if optimizer_type == 'adam':
+    optimizer = torch.optim.Adam(model.parameters(), lr = init_lr)
+criterion = torch.nn.CrossEntropyLoss().to(device)
 
-        return logit
-
-# 功能函数
-def train(model, iters, train_dataloader, val_dataloader, optimizer, criterion, log_interval, eval_interval):
-    iter = 0
-    model.train()
-    avg_loss_list = []
-    avg_acc_list = []
-    best_acc = 0.
-    while iter < iters:
-        for data in train_dataloader:
-            iter += 1
-            if iter > iters:
-                break
-            imgs = (data[0] / 255.).astype("float32")
-            labels = data[1].astype('int64')
-            # print(labels)
-            labels_ = paddle.unsqueeze(labels, axis=1)
-            logits = model(imgs) 
-            m = paddle.nn.Softmax()
-            pred = m(logits)
-            # print(pred.numpy())
-            # print(pred.numpy().argmax(1))            
-            acc = paddle.metric.accuracy(input=pred, label=labels_)
-            one_hot_labels = paddle.fluid.layers.one_hot(labels_, 2, allow_out_of_range=False)
-            loss = criterion(pred, one_hot_labels)            
-            # print(loss.numpy())
-            loss.backward()
-            optimizer.step()
-
-            model.clear_gradients()
-            avg_loss_list.append(loss.numpy()[0])
-            avg_acc_list.append(acc.numpy())
-            
-
-            if iter % log_interval == 0:
-                avg_loss = np.array(avg_loss_list).mean()
-                avg_acc = np.array(avg_acc_list).mean()
-                avg_loss_list = []
-                avg_acc_list = []
-                print("[TRAIN] iter={}/{} avg_loss={:.4f} avg_acc={:.4f}".format(iter, iters, avg_loss, avg_acc))
-
-            if iter % eval_interval == 0:
-                avg_loss, avg_acc = val(model, val_dataloader, criterion)
-                print("[EVAL] iter={}/{} avg_loss={:.4f} acc={:.4f}".format(iter, iters, avg_loss, avg_acc))
-                if avg_acc >= best_acc:
-                    best_acc = avg_acc
-                    paddle.save(model.state_dict(),
-                            os.path.join("/xx/task1_models/best_model_{:.4f}".format(best_acc), 'model.pdparams'))
-                model.train()
+train(model, iters, train_loader, val_loader, optimizer, criterion, log_interval=10, eval_interval=100, device=device)
 
 
-def val(model, val_dataloader, criterion):
-    model.eval()
-    avg_loss_list = []
-    avg_acc_list = []
-    cache = []
-    with paddle.no_grad():
-        for data in val_dataloader:
-            imgs = (data[0] / 255.).astype("float32")
-            labels = data[1].astype('int64')
-            labels_ = paddle.unsqueeze(labels, axis=1)
-            logits = model(imgs)
-            m = paddle.nn.Softmax()
-            pred = m(logits)            
-            acc = paddle.metric.accuracy(input=pred, label=labels_)
-            one_hot_labels = paddle.fluid.layers.one_hot(labels_, 2, allow_out_of_range=False)
-            loss = criterion(pred, one_hot_labels) 
-            avg_loss_list.append(loss.numpy()[0])
-            avg_acc_list.append(acc.numpy())        
+# # 预测阶段
+# best_model_path = '/xx/task1_models/xx/model.pdparams'
+# model = Model()
+# para_state_dict = paddle.load(best_model_path)
+# model.set_state_dict(para_state_dict)
+# model.eval()
 
-    avg_loss = np.array(avg_loss_list).mean()
-    acc = np.array(avg_acc_list).mean()
+# img_test_transforms = trans.Compose([
+#     trans.CropCenterSquare(),
+#     trans.Resize((image_size, image_size))
+# ])
 
-    return avg_loss, acc
+# test_dataset = GOALS_sub2_dataset(dataset_root=test_root, 
+#                         img_transforms=img_test_transforms,
+#                         mode='test')
+# cache = []
+# for img, idx in test_dataset:
+#     img = img[np.newaxis, ...]
+#     img = paddle.to_tensor((img / 255.).astype("float32"))
+#     logits = model(img) 
+#     m = paddle.nn.Softmax()
+#     pred = m(logits)
+#     print(pred.numpy())
+#     cache.append([idx, pred.numpy()[0][1]])
 
-
-# 训练阶段
-img_train_transforms = trans.Compose([
-    trans.RandomResizedCrop(
-        image_size, scale=(0.90, 1.1), ratio=(0.90, 1.1)),
-    trans.RandomHorizontalFlip(),
-    trans.RandomVerticalFlip(),
-    trans.RandomRotation(30)
-])
-
-
-img_val_transforms = trans.Compose([
-    trans.CropCenterSquare(),
-    trans.Resize((image_size, image_size))
-])
-
-train_dataset = GOALS_sub2_dataset(dataset_root=trainset_root, 
-                        img_transforms=img_train_transforms,
-                        filelists=train_filelists,
-                        label_file='/xx/Train/Train_GC_GT.xlsx')
-
-val_dataset = GOALS_sub2_dataset(dataset_root=trainset_root, 
-                        img_transforms=img_val_transforms,
-                        filelists=val_filelists,
-                        label_file='/xx/Train/Train_GC_GT.xlsx')
-
-train_loader = paddle.io.DataLoader(
-    train_dataset,
-    batch_sampler=paddle.io.DistributedBatchSampler(train_dataset, batch_size=batchsize, shuffle=True, drop_last=False),
-    num_workers=num_workers,
-    return_list=True,
-    use_shared_memory=False
-)
-
-val_loader = paddle.io.DataLoader(
-    val_dataset,
-    batch_sampler=paddle.io.DistributedBatchSampler(val_dataset, batch_size=batchsize, shuffle=True, drop_last=False),
-    num_workers=num_workers,
-    return_list=True,
-    use_shared_memory=False
-)
-
-model = Model()
-
-if optimizer_type == "adam":
-    optimizer = paddle.optimizer.Adam(init_lr, parameters=model.parameters())
-
-criterion = nn.BCEWithLogitsLoss()
-train(model, iters, train_loader, val_loader, optimizer, criterion, log_interval=10, eval_interval=100)
-
-
-# 预测阶段
-best_model_path = '/xx/task1_models/xx/model.pdparams'
-model = Model()
-para_state_dict = paddle.load(best_model_path)
-model.set_state_dict(para_state_dict)
-model.eval()
-
-img_test_transforms = trans.Compose([
-    trans.CropCenterSquare(),
-    trans.Resize((image_size, image_size))
-])
-
-test_dataset = GOALS_sub2_dataset(dataset_root=test_root, 
-                        img_transforms=img_test_transforms,
-                        mode='test')
-cache = []
-for img, idx in test_dataset:
-    img = img[np.newaxis, ...]
-    img = paddle.to_tensor((img / 255.).astype("float32"))
-    logits = model(img) 
-    m = paddle.nn.Softmax()
-    pred = m(logits)
-    print(pred.numpy())
-    cache.append([idx, pred.numpy()[0][1]])
-
-submission_result = pd.DataFrame(cache, columns=['ImgName', 'GC_Pred'])
-submission_result[['ImgName', 'GC_Pred']].to_csv("./submission_val_sub2.csv", index=False)
+# submission_result = pd.DataFrame(cache, columns=['ImgName', 'GC_Pred'])
+# submission_result[['ImgName', 'GC_Pred']].to_csv("./submission_val_sub2.csv", index=False)
